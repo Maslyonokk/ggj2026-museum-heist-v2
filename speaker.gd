44de977fff10e6af040da61608e6da3566d95b9d
Alexandru <vasilaki.alexander@gmail.com>
2026-02-01 20:56:43 +0100

    Speaker now requires just one click, instead of needing to be held down to work

--- speaker.gd
+++ speaker.gd
@@ -6,19 +6,26 @@ signal speaker_stopped
 
 
 @onready var animation : AnimatedSprite2D = get_node("AnimatedSprite2D")
+@onready var timer : Timer = get_node("Timer")
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				animation.play()
-				speaker_playing.emit()
-			else:
-				animation.stop()
-				speaker_stopped.emit()
+		animation.play()
+		speaker_playing.emit()
+		timer.start()
+
+				#animation.stop()
+				#speaker_stopped.emit()
 
 
 
 func _on_mouse_exited() -> void:
+	pass
+
+	
+	
+
+
+func _on_timer_timeout() -> void:
 	animation.stop()
 	speaker_stopped.emit()
